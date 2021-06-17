e9e34da682311251bc76debc425a0cebd78d9fd8
Constantkavaya <kavayaconstant@gmail.comgit config --listgit config --global user.email kavayaconstant@gmail.com>
2021-06-17 11:42:55 +0300

    quiz

---
+++ python_test.py
@@ -0,0 +1,52 @@
+x=[100,110,120,130,140,150]
+for y*5 ,y in x:
+      x*=y
+      print(x)
+
+def divisible_by_three(n):
+ for x in range (1, n):
+    if x%3==0:
+        print(x)
+    divisible_by_three(20)
+
+def numbers():
+    x=[[1,2],[3,4],[5,6]]
+    print(x.sort())
+    def smallest():
+        s=[2,0,5,4,6,7,8]
+        print(min(s))
+    def remove():
+        x=['a','b','a','e','d','b','c','e','f','g','h']
+        print(set(x))
+
+        def divisible_by_seven():
+            x=range(100,200)
+            for y in x:
+                if y%7==0:
+                    print(y)
+        divisible_by_seven() 
+students = [{"age": 19, "name": "Eunice"}, {"age": 21, "name": "Agnes"},{"age": 18, "name": "Teresa"}, {"age": 22, "name": "Asha"}]
+def greet():
+    for learn in students:
+        print("Hello {}, you were born in{}".format(learn['name'],2021-learn['age']))
+
+greet()
+
+class Rectangle():
+    def  __init__(self,width,length) :
+        self.width=width
+        self.length=length
+        A=0
+
+
+
+
+
+       
+
+
+
+
+
+
+
